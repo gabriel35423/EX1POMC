f288e4585f27110b91eb2ca5d27beab20e154a55
gabriel35423 <137359307+gabriel35423@users.noreply.github.com>
2023-09-04 18:50:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX1POOMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write( "Digite a base do retângulo:");
+
+            Double baseInput = Double.Parse(Console.ReadLine());
+
+
+
+            Console.Write( "Digite a altura do retângulo:");
+
+            Double alturaInput = Double.Parse(Console.ReadLine());
+
+
+
+            Retangulo retangulo = new Retangulo(baseInput, alturaInput);
+
+            Double area = retangulo.CalcularArea();
+
+
+
+            Console.WriteLine("A área do retângulo é: {0}", area);
+        }
+    }
+}
